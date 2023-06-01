ea37a31ce9f5b728912e5bc049737c3f7dfc6abc
Ygor <ygor_fernandes1996@hotmail.com>
2023-06-01 01:54:41 -0300

    Realizand entrega da semana passada

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,21 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal(){
+    const button = document.querySelector('#buttonModal');
+    const modal = document.querySelector('.modal__controller');
+
+    button.addEventListener('click', function(e){
+        modal.showModal();
+        closeModal();
+    });
+}
+
+function closeModal() {
+    const button = document.querySelector('#buttonClose');
+    const modal = document.querySelector('.modal__controller'); 
+
+    button.addEventListener('click', function(e){
+        modal.close();
+    });
+}
+
+handleModal();
